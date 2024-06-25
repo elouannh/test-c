8264a36c7d5c98cd11d630e23384b04a0433e471
elouannh <elouannhosta27@gmail.com>
2024-06-25 20:57:47 +0200

    Ajout d'une addition

--- main.c
+++ main.c
@@ -1,3 +1,11 @@
-int main() {
-    return 0;
+#include<stdio.h>
+
+int addition(int a, int b) {
+    int c = a + b;
+    return c;
+}
+
+void main() {
+    int result = addition(4, 5);
+    printf("%d", result);
 }
---
+++ main.s
@@ -0,0 +1,57 @@
+	.file	"main.c"
+	.text
+	.globl	addition
+	.type	addition, @function
+addition:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	%edi, -4(%rbp)
+	movl	%esi, -8(%rbp)
+	movl	-4(%rbp), %edx
+	movl	-8(%rbp), %eax
+	addl	%edx, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	addition, .-addition
+	.section	.rodata
+.LC0:
+	.string	"%d"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB1:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	$5, %esi
+	movl	$4, %edi
+	call	addition
+	movl	%eax, -4(%rbp)
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	nop
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	main, .-main
+	.ident	"GCC: (Debian 12.2.0-14) 12.2.0"
+	.section	.note.GNU-stack,"",@progbits
